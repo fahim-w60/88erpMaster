2d03380a301631cc4f1ad040629ad4bec31fac9b
FerdousPulok1 <pulok@88innovationsltd.com>
2024-06-10 18:00:28 +0600

    Roster Group Update form 60% done

--- package/eight_hrm/src/routes/web.php
+++ package/eight_hrm/src/routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use Innovation\EightHrm\Http\Controllers\HrmDashBoardController;
 use Innovation\EightHrm\Http\Controllers\UserReportController;
 use Innovation\EightHrm\Http\Controllers\attendance\AttendanceReportController;
+use Innovation\EightHrm\Http\Controllers\Leave\AdminLeaveEntryController;
+use Innovation\EightHrm\Http\Controllers\attendance\RosterGroupUpdateController; 
+
 
 Route::group(['prefix' => 'hrm'], function () {
     Route::get('dashboard', [HrmDashBoardController::class,'index'])->name('hrm-dashboard');
@@ -17,6 +20,13 @@ Route::group(['prefix' => 'hrm'], function () {
     //Attendance Controller
     //Route::resource('attendance', AttendanceReportController::class);
     Route::get('daily-attendance', [AttendanceReportController::class, 'daily_attendance'])->name('attendance.daily_attendance');
+
+
+    /*Leave Management URLS*/
+    Route::resource('admin-leave-entry', AdminLeaveEntryController::class);
+    Route::resource('roster-group-update', RosterGroupUpdateController::class);
+ 
+
 
 
 });
