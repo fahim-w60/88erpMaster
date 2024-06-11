17157dc07a37c17f9a40d449700df1033232ded7
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2024-06-11 13:24:25 +0600

    e-recruitment route update

--- package/eight_hrm/src/routes/atiq_web.php
+++ package/eight_hrm/src/routes/atiq_web.php
@@ -3,8 +3,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Innovation\EightHrm\Http\Controllers\employee\EmployeeInfoController;
+use Innovation\EightHrm\Http\Controllers\e_recruitment\InterviewController;
+use Innovation\EightHrm\Http\Controllers\e_recruitment\InterviewEvaluationController;
 
 Route::group(['prefix' => 'hrm'], function () {
    Route::resource('employee', EmployeeInfoController::class);
+    // E-Recruitment
+    Route::resource('interview', InterviewController::class);
+    Route::resource('interview-evaluation', InterviewController::class);
  
 });
--- package/eight_hrm/src/routes/web.php
+++ package/eight_hrm/src/routes/web.php
@@ -10,9 +10,7 @@ use Innovation\EightHrm\Http\Controllers\Leave\AdminLeaveEntryController;
 use Innovation\EightHrm\Http\Controllers\attendance\RosterGroupUpdateController; 
 
 
-use Innovation\EightHrm\Http\Controllers\employee\EmployeeInfoController;
-use Innovation\EightHrm\Http\Controllers\e_recruitment\InterviewController;
-use Innovation\EightHrm\Http\Controllers\e_recruitment\InterviewEvaluationController;
+
 
 Route::group(['prefix' => 'hrm'], function () {
    
@@ -25,10 +23,7 @@ Route::group(['prefix' => 'hrm'], function () {
     Route::get('summary-attendance', [AttendanceReportController::class, 'summary_attendance'])->name('attendance.summary_attendance');
     Route::get('job-card', [AttendanceReportController::class, 'job_card'])->name('attendance.job_card');
     Route::get('punch-record', [AttendanceReportController::class, 'punch_record'])->name('attendance.punch_record');
-    Route::resource('employee', EmployeeInfoController::class);
-    // E-Recruitment
-    Route::resource('interview', InterviewController::class);
-    Route::resource('interview-evaluation', InterviewController::class);
+
 
 
     /*Leave Management URLS*/
