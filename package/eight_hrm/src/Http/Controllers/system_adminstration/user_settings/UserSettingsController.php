61197293bb3a9ca75fbb5036e2ff95854bcb1d16
fahim-w60 <fahimlxp@gmail.com>
2024-06-12 17:52:00 +0600

    tanim

---
+++ package/eight_hrm/src/Http/Controllers/system_adminstration/user_settings/UserSettingsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Innovation\EightHrm\Http\Controllers\system_adminstration\user_settings;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserSettingsController extends Controller
+{
+    
+}
--- package/eight_hrm/src/views/layouts/aside.blade.php
+++ package/eight_hrm/src/views/layouts/aside.blade.php
@@ -93,7 +93,8 @@
           </li> --}}
           
          
-        
+
+          
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon far fa-plus-square"></i>
